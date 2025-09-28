6349a40441974ad41b0db001e8666404e6799136
SamsonLuisa <luisa.samson@yahoo.com>
2025-09-28 12:17:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,37 +16,3 @@ function closeNavMobile() {
 
 
 
-
-/*
-function openNav() {
-  document.getElementById("mySidebar").style.width = "250px"; 
-  document.getElementById("main").style.marginLeft = "250px";
-
-  
-}
-
-function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
-}
-*/
-
-
-
-/*
-
-function toggleNav() {
-  const sidebar = document.getElementById("meniu");
-  const main = document.getElementById("continut");
-
-  if (sidebar.style.width === "250px") {
-    // Sidebar e deschis → îl închidem
-    sidebar.style.width = "0";
-    main.style.marginLeft = "0";
-  } else {
-    // Sidebar e închis → îl deschidem
-    sidebar.style.width = "25%";
-    main.style.marginLeft = "250px";
-  }
-}
-*/
